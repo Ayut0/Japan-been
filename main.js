e099ae6043f0b17470183796e6787f6b000646c2
Ayut0 <yuvto.0420pgmail.com>
2021-01-08 21:53:46 +0900

    【JS】サーチモーダルのリアルタイム検索機能を実装

--- main.js
+++ main.js
@@ -248,7 +248,7 @@
     '沖縄'
   ];
 
-  // 県一覧をを追加
+  // 県一覧を追加
   (function(){
     const prefFragment = document.createDocumentFragment();
     
@@ -276,7 +276,36 @@
     }
   },false);
 
+
+  // モーダル内のリアルタイム絞り込み機能
+  // let $lists = document.getElementsByClassName('searchPref');
+  // console.log($lists);
+  // let li = $lists[0].innerHTML.toLocaleUpperCase();
+  // console.log(li);
   
+    
+    let searchInput = document.getElementById('search-input'); //モーダル内の検索フォームを取得
+    searchInput.addEventListener('input' , function(){
+    let filter = searchInput.value.toUpperCase(); //入力された文字を取得
+    let $lists = document.getElementsByClassName('searchPref'); //モーダル内の各県（li）を取得
+    // console.log(filter);
+    for(let i = 0;i<$lists.length;i++){ //$lists.length は47
+      let li = $lists[i].innerHTML; //i番目の県を取得
+      if(li){
+        if(li.toUpperCase().indexOf(filter) > -1){
+          $lists[i].style.display = '';
+        }else{
+          $lists[i].style.display = 'none';
+        }
+      }
+  };
+});
+  
+  
+    
+
+  
+  // モーダル関係はclassList.toggleで書き換えられそう？
   // アイコンをクリックしてモーダルを開く
   search.addEventListener('click',function(){
     // モーダルの表示
